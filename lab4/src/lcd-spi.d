a746e0cbab612b96419fde038bc438601d40daa7
Andrés Artavia <112042541+artavias@users.noreply.github.com>
2024-10-30 23:55:00 -0600

    Ordenando archivos

---
+++ lab4/src/lcd-spi.d
@@ -0,0 +1,26 @@
+lcd-spi.o: lcd-spi.c /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/spi.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/spi.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/spi_common_v1_frf.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/spi_common_v1.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/spi_common_all.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/pwr.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/gpio.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/cm3/nvic.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /mnt/c/Users/navar/OneDrive/eliminar/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/nvic.h \
+ console.h clock.h sdram.h lcd-spi.h
